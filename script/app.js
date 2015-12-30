ae1723a9dac6417fc94af20ec01f81341bd4f325
Loïc Parent <loic1994@hotmail.com>
2015-12-31 00:25:37 +0100

    corrected the code
    => check the size of header
    => check if home
    …

--- script/app.js
+++ script/app.js
@@ -3,47 +3,49 @@
 function isLoad(){
 	( function( $ ){
 
-		var sections = [],
-			id = false,
-			headerSize = 100;
+		if ( $('.topPresentation__title1').length !== 0  ) {
+			var sections = [],
+				id = false;
+				window.headerSize = $('.header').height();
 
-		$( '.mainMenu a' ).each( function(){
-			sections.push( $( $( this ).attr( 'href' ) ) );
-		} );
+			$( '.mainMenu a' ).each( function(){
+				sections.push( $( $( this ).attr( 'href' ) ) );
+			} );
 
-		$( window ).scroll( function(e){
-			// récupérer la moitié de l'écran
-			var scrollTop =  $( this ).scrollTop() + ( $( window ).height() / 2 );
-			var upScroll = $( this ).scrollTop();
-			for( var i in sections ){
-				var section = sections[i];
-				if( scrollTop > section.offset().top ){
-					var scrolled_id = section.attr( "id" );
+			$( window ).scroll( function(e){
+				// récupérer la moitié de l'écran
+				var scrollTop =  $( this ).scrollTop() + ( $( window ).height() / 2 );
+				var upScroll = $( this ).scrollTop();
+				for( var i in sections ){
+					var section = sections[i];
+					if( scrollTop > section.offset().top ){
+						var scrolled_id = section.attr( "id" );
+					}
 				}
-			}
-			var headerHeight = $( ".header" ).height();
-			headerSize = headerHeight;
-			if( upScroll >= $( '#aboutMe' ).offset().top-headerHeight ){    // si le scroll est plus grand que le dessus de la section about + 100 px (pour le header)
-				$( '.header__logo' ).addClass( 'show' );
-			} else {
-				$( '.header__logo' ).removeClass( 'show' );
-			}
-			if( scrolled_id !== id ){
-				id = scrolled_id;
-				$( '.mainMenu a' ).removeClass( 'active' );
-				$( '.mainMenu a[href="#'+ id +'"]' ).addClass( 'active' );
-			}
-		} );
 
-		//petit script pour le smoothMove:
-		var headerSize = $('.header').height;
+				if( upScroll >= $( '#aboutMe' ).offset().top-headerSize ){
+					$( '.header__logo' ).addClass( 'show' );
+				} else {
+					$( '.header__logo' ).removeClass( 'show' );
+				}
+				if( scrolled_id !== id ){
+					id = scrolled_id;
+					$( '.mainMenu a' ).removeClass( 'active' );
+					$( '.mainMenu a[href="#'+ id +'"]' ).addClass( 'active' );
+				}
+			} );
 
-		$('.mainMenu a').click(function(){
-		    $('html, body').animate({
-		        scrollTop: $( $.attr(this, 'href') ).offset().top-headerSize
-		    }, 500);
-		    return false;
-		});
+
+			//petit script pour le smoothMove:
+			$('.mainMenu a').click(function(){
+			    $('html, body').animate({
+			        scrollTop: $( $.attr(this, 'href') ).offset().top-headerSize
+			    }, 500);
+			    return false;
+			});	
+		} else {
+			$( '.header__logo' ).addClass( 'show' );
+		}
 
 	} )( jQuery );
 }
