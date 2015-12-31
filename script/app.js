a463e75cca99e52ae7dc0516d2255116d6f194b7
Loïc Parent <loic1994@hotmail.com>
2015-12-31 11:18:25 +0100

    Add a focus script

--- script/app.js
+++ script/app.js
@@ -1,6 +1,28 @@
 // Script de Grafikart pour la gestion du changement de la classe active du menu.
 
 function isLoad(){
+
+	//1: Gestion du focus sur les articles.
+	$(".project a").focusin(function(e){
+	    $(this).next().css({
+	    	"-webkit-transform": "translateY(0%)",
+	    	"-moz-transform": "translateY(0%)",
+	    	"-ms-transform": "translateY(0%)",
+	    	"-o-transform": "translateY(0%)",
+	    	"transform": "translateY(0%)"
+	    });
+	});
+
+	$(".project a").focusout(function(e){
+	    $(this).next().css({
+	    	"-webkit-transform": "translateY(100%)",
+	    	"-moz-transform": "translateY(100%)",
+	    	"-ms-transform": "translateY(100%)",
+	    	"-o-transform": "translateY(100%)",
+	    	"transform": "translateY(100%)"
+	    });
+	});
+
 	( function( $ ){
 
 		if ( $('.topPresentation__title1').length !== 0  ) {
