ec97af264f1a5952182ad6e03bfccb2a967bd593
Loïc Parent <loic1994@hotmail.com>
2016-01-18 11:27:32 +0100

    Add a sitemap file

---
+++ sitemap.php
@@ -0,0 +1,35 @@
+<?php 
+$pageTitle = "Loïc Parent • Plan du site";
+include( 'header.php' );
+?>
+
+<h1 class="title2">Plan du site</h1>
+<ul id="sitemap" class="centerZone sitemap">
+	<li><a href="./index.php">Accueil</a>
+		<ul>
+			<li><a href="./index.php#aboutMe">À propos</a></li>
+			<li><a href="./index.php#works">Mes projets</a>
+				<ul>
+					<li><a href="http://stucher.loic-parent.be">Stucher (PFE)</a></li>
+					<li><a href="./alfa.php">Centre Alfa</a></li>
+					<li><a href="./bibliotheque.php">Bibliothèque en ligne</a></li>
+					<li><a href="./dswallonie.php">Ds Wallonie</a></li>
+					<li><a href="./esquive.php">MiniJeu Esquive</a></li>
+					<li><a href="./bouvachon.php">Bouvachon</a></li>
+					<li><a href="./stimform.php">Catalogue Stim-form</a></li>
+					<li><a href="./btf.php">Affiche Retour vers le futur</a></li>
+					<li><a href="./pancakes.php">Pancakes Typography</a></li>
+					<li><a href="./tshirt.php">Visuels de T-shirt</a></li>
+					<li><a href="./euroskills.php">Euroskills</a></li>
+					<li><a href="./tpcv.php">Touche pas mes certificas vert</a></li>
+					<li><a href="./rollstyle.php">Festival Roll-Style</a></li>
+					<li><a href="">…</a></li>
+				</ul>
+			</li>
+			<li><a href="./index.php#contact">Contact</a></li>
+		</ul>
+	</li>
+	<li><a href="#">Plan du site</a></li>
+</ul>
+
+<?php include( 'footer.php' ); ?>
