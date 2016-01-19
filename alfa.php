5fee292799aaf0631b9e3495f7e5ca57c988315b
Loïc Parent <loic1994@hotmail.com>
2016-01-19 04:55:49 +0100

    change content of project

---
+++ alfa.php
@@ -0,0 +1,28 @@
+<?php 
+$pageTitle = "Loïc Parent • Centre Alfa";
+include( 'header.php' );
+?>
+
+<h1 class="projectTitle-1 title2">Centre Alfa</h1>
+<div id="content" class="content">
+	<section class="infoProject">
+		<div class="centerZone">
+			<h2 class="projectTitle title2 hidden">Informations sur le projet</h2>
+			<div class="left">
+				<div class="pictureZone">
+					<img src="./img/alfa.png" alt="Centre Alfa">
+				</div>
+				<a href="http://alfa.loic-parent.be" class="infoProject__link">Accéder au site</a>
+			</div>
+			<div class="right">
+				<p>En troisième année de mon cursus à la Haute &Eacute;cole de la Province de Liège, j’ai participé à un coucours proposé par l’asbl Centre Alfa située à Liège.</p>
+				<p>Pour ce concours, nous devions proposer une refonte complète de leur site internet avec Wordpress et donc leurs proposer un design remis aux goût du jour.</p>
+				<p>J'ai été sélectionné par l'asbl et donc ai pu continuer le travail avec cette eux.</p>
+				<p>Trêve d’explications, je vous propose de découvrir le projet.</p>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</section>
+</div>
+
+<?php include( 'footer.php' ); ?>
---
+++ stucher.php
@@ -0,0 +1,28 @@
+<?php 
+$pageTitle = "Loïc Parent • Stucher (PFE)";
+include( 'header.php' );
+?>
+
+<h1 class="projectTitle-1 title2">Stucher PFE</h1>
+<div id="content" class="content">
+	<section class="infoProject">
+		<div class="centerZone">
+			<h2 class="projectTitle title2 hidden">Informations sur le projet</h2>
+			<div class="left">
+				<div class="pictureZone">
+					<img src="./img/stucher.png" alt="Stucher PFE">
+				</div>
+				<a href="http://stucher.loic-parent.be" class="infoProject__link">Accéder au site</a>
+			</div>
+			<div class="right">
+				<p>Me voici en dernière année du cursur à la Haute École de la Province de Liège et donc, je dois réaliser un Projet de fin d'étude.</p>
+				<p>Ce projet, c'est Stucher (pour Student and Teacher). Il s'agit d'un journal de classe interactif et partagé entre l'élève et son professeur.</p>
+				<p>Pour le moment il s'agit d'une version BETA du projet.</p>
+				<p>Trêve d’explications, je vous propose de découvrir le projet.</p>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</section>
+</div>
+
+<?php include( 'footer.php' ); ?>
