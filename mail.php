a7fbb5febb19a42111e18b5ba65baefce0384b7e
Loïc Parent <loic1994@hotmail.com>
2016-01-26 16:07:05 +0100

    Check if it's a real number

--- mail.php
+++ mail.php
@@ -1,7 +1,12 @@
 <?php 
 	
 	function isRealNumber() {
-		return false;
+		$string = $_POST['phone'];
+		if (preg_match('/^\(?[0-9]{3}\)?|[0-9]{3}[-. ]? [0-9]{3}[-. ]?[0-9]{4}$/', $string)) { 
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	if ( isset($_POST['formsent']) ) {
