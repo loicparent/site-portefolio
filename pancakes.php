57f58626ad57e9ff2e961b44ea1cd00c89c9562c
Loïc Parent <loic1994@hotmail.com>
2016-02-29 21:45:23 +0100

    change gallery

--- pancakes.php
+++ pancakes.php
@@ -25,83 +25,83 @@ include( 'header.php' );
 			</div>
 			<div class="clear"></div>
 		</div>
-		<div class="description pancakesDesc">
-			<a class="image-popup-no-margins" href="./img/pancakes/a.jpg">
+		<div class="zoom-gallery description pancakesDesc">
+			<a href="./img/pancakes/a.jpg">
 				<img src="./img/pancakes/a.jpg" alt="Photo pancakes lettre A">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/b.jpg">
+			<a href="./img/pancakes/b.jpg">
 				<img src="./img/pancakes/b.jpg" alt="Photo pancakes lettre B">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/c.jpg">
+			<a href="./img/pancakes/c.jpg">
 				<img src="./img/pancakes/c.jpg" alt="Photo pancakes lettre C">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/d.jpg">
+			<a href="./img/pancakes/d.jpg">
 				<img src="./img/pancakes/d.jpg" alt="Photo pancakes lettre D">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/e.jpg">
+			<a href="./img/pancakes/e.jpg">
 				<img src="./img/pancakes/e.jpg" alt="Photo pancakes lettre E">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/f.jpg">
+			<a href="./img/pancakes/f.jpg">
 				<img src="./img/pancakes/f.jpg" alt="Photo pancakes lettre F">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/g.jpg">
+			<a href="./img/pancakes/g.jpg">
 				<img src="./img/pancakes/g.jpg" alt="Photo pancakes lettre G">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/h.jpg">
+			<a href="./img/pancakes/h.jpg">
 				<img src="./img/pancakes/h.jpg" alt="Photo pancakes lettre H">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/i.jpg">
+			<a href="./img/pancakes/i.jpg">
 				<img src="./img/pancakes/i.jpg" alt="Photo pancakes lettre I">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/j.jpg">
+			<a href="./img/pancakes/j.jpg">
 				<img src="./img/pancakes/j.jpg" alt="Photo pancakes lettre J">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/k.jpg">
+			<a href="./img/pancakes/k.jpg">
 				<img src="./img/pancakes/k.jpg" alt="Photo pancakes lettre K">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/l.jpg">
+			<a href="./img/pancakes/l.jpg">
 				<img src="./img/pancakes/l.jpg" alt="Photo pancakes lettre L">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/m.jpg">
+			<a href="./img/pancakes/m.jpg">
 				<img src="./img/pancakes/m.jpg" alt="Photo pancakes lettre M">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/n.jpg">
+			<a href="./img/pancakes/n.jpg">
 				<img src="./img/pancakes/n.jpg" alt="Photo pancakes lettre N">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/o.jpg">
+			<a href="./img/pancakes/o.jpg">
 				<img src="./img/pancakes/o.jpg" alt="Photo pancakes lettre O">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/p.jpg">
+			<a href="./img/pancakes/p.jpg">
 				<img src="./img/pancakes/p.jpg" alt="Photo pancakes lettre P">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/q.jpg">
+			<a href="./img/pancakes/q.jpg">
 				<img src="./img/pancakes/q.jpg" alt="Photo pancakes lettre Q">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/r.jpg">
+			<a href="./img/pancakes/r.jpg">
 				<img src="./img/pancakes/r.jpg" alt="Photo pancakes lettre R">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/s.jpg">
+			<a href="./img/pancakes/s.jpg">
 				<img src="./img/pancakes/s.jpg" alt="Photo pancakes lettre S">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/t.jpg">
+			<a href="./img/pancakes/t.jpg">
 				<img src="./img/pancakes/t.jpg" alt="Photo pancakes lettre T">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/u.jpg">
+			<a href="./img/pancakes/u.jpg">
 				<img src="./img/pancakes/u.jpg" alt="Photo pancakes lettre U">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/v.jpg">
+			<a href="./img/pancakes/v.jpg">
 				<img src="./img/pancakes/v.jpg" alt="Photo pancakes lettre V">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/w.jpg">
+			<a href="./img/pancakes/w.jpg">
 				<img src="./img/pancakes/w.jpg" alt="Photo pancakes lettre W">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/x.jpg">
+			<a href="./img/pancakes/x.jpg">
 				<img src="./img/pancakes/x.jpg" alt="Photo pancakes lettre X">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/y.jpg">
+			<a href="./img/pancakes/y.jpg">
 				<img src="./img/pancakes/y.jpg" alt="Photo pancakes lettre Y">
 			</a>
-			<a class="image-popup-no-margins" href="./img/pancakes/z.jpg">
+			<a href="./img/pancakes/z.jpg">
 				<img src="./img/pancakes/z.jpg" alt="Photo pancakes lettre Z">
 			</a>
 		</div>
